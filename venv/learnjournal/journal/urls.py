7151282f189e0c916c45963d4cfaf17932b0e15f
Kieran Smith <kieransmith63@gmail.com>
2022-03-08 17:18:03 +0000

    Create resource form

--- venv/learnjournal/journal/urls.py
+++ venv/learnjournal/journal/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import (ResourceView)
+from .views import (ResourceView, ResourceCreateView)
 from . import views
 
 urlpatterns = [
-    path('', ResourceView.as_view(), name='home')
+    path('', ResourceView.as_view(), name='home'),
+    path('resource/new/', ResourceCreateView.as_view(), name="new-resource")
 ]
--- venv/learnjournal/journal/views.py
+++ venv/learnjournal/journal/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import (ListView)
+from django.views.generic import (ListView, CreateView)
 from .models import Resource
 
 # Create your views here.
@@ -19,3 +19,11 @@ class ResourceView(ListView):
     model = Resource
     template_name = 'journal/home.html'
     context_object_name = 'resources'
+
+
+class ResourceCreateView(CreateView):
+    model = Resource
+    fields = ['name', 'link']
+
+    def form_valid(self, form):
+        return super().form_valid(form)
