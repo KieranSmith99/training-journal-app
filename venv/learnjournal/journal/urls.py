4643e31e8f05a0a04d13b750ef82dc446efc4fdc
Kieran Smith <kieransmith63@gmail.com>
2022-03-08 15:15:33 +0000

    Create Resource view and new url pattern

--- venv/learnjournal/journal/urls.py
+++ venv/learnjournal/journal/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 
-from .views import ()
+from .views import (ResourceView)
 from . import views
+
+urlpatterns = [
+    path('', views.home, name='home')
+]
--- venv/learnjournal/journal/views.py
+++ venv/learnjournal/journal/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .models import Resource
 
 # Create your views here.
+
+
+def home(request):
+    context = {
+        'resources': Resource.objects.all()
+    }
+
+    return render(request, 'journal/home.html', context)
+
+
+class ResourceView(View):
+    model = Resource
+    template_name = 'journal/home.html'
+    context_object_name = 'resources'
