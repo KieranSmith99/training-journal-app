a356e39e7c578b1e9446f9273c337b8ceb3870e1
Kieran Smith <kieransmith63@gmail.com>
2022-03-09 16:04:35 +0000

    Made all resource fields except name optional

--- venv/learnjournal/journal/models.py
+++ venv/learnjournal/journal/models.py
@@ -47,10 +47,10 @@ class Technology(models.Model):
 class Resource(models.Model):
     name = models.CharField(max_length=100)
     link = models.URLField(max_length=100, blank=True)
-    language = models.ForeignKey(Language, on_delete=models.CASCADE)
-    framework = models.ForeignKey(Framework, on_delete=models.CASCADE)
-    database = models.ForeignKey(Database, on_delete=models.CASCADE)
-    technology = models.ForeignKey(Technology, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE, blank=True, null=True)
+    framework = models.ForeignKey(Framework, on_delete=models.CASCADE, blank=True, null=True)
+    database = models.ForeignKey(Database, on_delete=models.CASCADE, blank=True, null=True)
+    technology = models.ForeignKey(Technology, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
