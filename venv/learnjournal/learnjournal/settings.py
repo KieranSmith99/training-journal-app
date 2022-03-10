74add87a25241368964da6632d68167eaed1fd40
Kieran Smith <kieransmith63@gmail.com>
2022-03-10 13:28:34 +0000

    Use MySql database

--- venv/learnjournal/learnjournal/settings.py
+++ venv/learnjournal/learnjournal/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'learnjournal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_database',
+        'USER': 'dbadmin',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
