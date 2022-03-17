29d8fd95bbf533b6316912952774b195462b92a3
Kieran Smith <kieransmith63@gmail.com>
2022-03-17 13:34:12 +0000

    Refactor again

--- venv/learnjournal/learnjournal/settings.py
+++ venv/learnjournal/learnjournal/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from .secret_things import SECRET_KEY_SAFE
+from python-decouple import config
 from google.oauth2 import service_account
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY_SAFE
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
